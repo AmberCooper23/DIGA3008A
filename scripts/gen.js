1a4b2e95cfbeda91b036ea0b1413b82b59df34fe
AmberCooper23 <2677396@students.wits.ac.za>
2025-05-23 11:16:52 +0200

    test dark mode + fix to top button 5

--- scripts/gen.js
+++ scripts/gen.js
@@ -4,38 +4,33 @@ const modeToggle = document.getElementById("modeToggle");
 if (toTopBtn) {
     window.onscroll = function () {
         if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-            toTopBtn.style.display = "block";
-        }
-        else {
+           toTopBtn.style.display = "block";
+        } else {
             toTopBtn.style.display = "none";
         }
-};
+    };
 
-toTopBtn.onclick = function () {
-    window.scrollTo({top: 0, behavior: "smooth"});
-};
-
+    toTopBtn.onclick = function () {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme === "dark") {
-        document.body.classList.add("darkMode");
-        if (modeToggle) {
-            modeToggle.checked = true;
-        }
-
-    }
-})
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark-mode");
+    if (modeToggle) modeToggle.checked = true;
+  }
+});
 
 if (modeToggle) {
-modeToggle/addEventListener("change", function () {
+  modeToggle.addEventListener("change", function () {
     if (this.checked) {
-        document.body.classList.add("darkMode");
-        localStorage.setItem("theme", "dark");
+      document.body.classList.add("dark-mode");
+      localStorage.setItem("theme", "dark");
     } else {
-        document.body.classList.remove("darkMode");
-        localStorage.setItem("theme", "light");
+      document.body.classList.remove("dark-mode");
+      localStorage.setItem("theme", "light");
     }
-});
+  });
 }
